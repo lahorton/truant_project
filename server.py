fc1fe5df473e73bdee0395304b7309d36ed589ad
lahorton <lahorton@gmail.com>
2018-09-02 18:37:07 +0000

    basic outline for profile site

---
+++ server.py
@@ -0,0 +1,47 @@
+from flask import (Flask, render_template, redirect, request, flash, session)
+from jinja2 import StrictUndefined
+from flask import Flask
+from flask_debugtoolbar import DebugToolbarExtension
+import os
+from model import connect_to_db, db
+
+app = Flask(__name__)
+
+# Required to use Flask sessions and the debug toolbar
+app.secret_key = os.environ["SERVER_APP_SECRET_KEY"]
+
+# Normally, if you use an undefined variable in Jinja2, it fails silently.
+# This is horrible. Fix this so that, instead, it raises an error.
+app.jinja_env.undefined = StrictUndefined
+
+
+@app.route('/')
+def index():
+    """Homepage."""
+
+    return "<html><body>HOMEPAGE!</body></html>"
+
+@app.route('/about')
+def tells_about():
+    """all about J"""
+
+    return "<html><body>ABOUT!</body></html>"
+
+@app.route('/pricing')
+def gives_pricing():
+    """provides overview of ordering and pricing"""
+
+    return "<html><body>Pricing!</body></html>"
+
+
+if __name__ == "__main__":
+    # We have to set debug=True here, since it has to be True at the point
+    # that we invoke the DebugToolbarExtension
+    app.debug = True
+
+    connect_to_db(app)
+
+    # Use the DebugToolbar
+    DebugToolbarExtension(app)
+
+    app.run(host="0.0.0.0")
